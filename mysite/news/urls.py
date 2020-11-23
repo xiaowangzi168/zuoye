6d7ea83db5521803a4af61ad511b7fd5ad556453
xiaowangzi168 <1023270305@qq.com>
2020-11-23 15:24:03 +0800

    请教老师后的修改

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hw/create/', views.Homework(CreateView)),
+    path('hw/create/', views.HomeworkCreate.as_view()),
     #path('articles/<int:year>/<int:month>/', views.month_archive),
     #path('articles/<int:year>/<int:month>/<int:pk>/', views.article_detail),
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -9,8 +9,8 @@ def year_archive(request, year):
 '''
 from .models import Student, Homework
 from django.views.generic.edit import CreateView
+
 class HomeworkCreate(CreateView):
     model = Homework
     template_name = 'homework_form.html'
     fields = ['commit_date','headline','attach','remark','student']
-    
